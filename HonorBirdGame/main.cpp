db755c8cc8034ed07d7d6df7841d6a69caaaae79
方成武 <fangchengwu863@163.com>
2026-03-28 16:40:18 +0800

    feat:add drag launch prototype and notes

--- HonorBirdGame/main.cpp
+++ HonorBirdGame/main.cpp
@@ -10,11 +10,11 @@ int main()
 	ground.setFillColor(sf::Color(80, 180, 80));
 
 	sf::RectangleShape slingPost(sf::Vector2f(12.f, 70.f));
-	slingPost.setPosition(60.f, 410.f);
+	slingPost.setPosition(60.f, 290.f);
 	slingPost.setFillColor(sf::Color(139, 69, 19));
 
 	sf::CircleShape bird(20.f);
-	bird.setPosition(80.f, 440.f);
+	bird.setPosition(80.f, 320.f);
 	bird.setFillColor(sf::Color::Red);
 	bird.setOutlineThickness(3.f);
 	bird.setOutlineColor(sf::Color::Black);
@@ -25,12 +25,18 @@ int main()
 	enemy.setOutlineThickness(3.f);
 	enemy.setOutlineColor(sf::Color::Black);
 
+	sf::Vector2f birdStartPos(80.f, 320.f);
+
 	bool birdReady = true;
 	bool enemyHit = false;
 	bool enemyAlive = true;
+	bool isDragging = false;
+
 	float birdSpeedX = 0.f;
 	float birdSpeedY = 0.f;
 	float gravity = 0.0012f;
+	float maxDragDistance = 120.f;
+	float launchPower = 0.03f;
 
 	// ===== 主循环 =====
 	while (window.isOpen())
@@ -55,8 +61,9 @@ int main()
 
 				if (event.key.code == sf::Keyboard::R)
 				{
-					bird.setPosition(80.f, 440.f);
+					bird.setPosition(birdStartPos);
 					birdReady = true;
+					isDragging = false;
 					birdSpeedX = 0.f;
 					birdSpeedY = 0.f;
 					bird.setFillColor(sf::Color::Red);
@@ -64,9 +71,57 @@ int main()
 					enemyHit = false;
 					enemyAlive = true;
 					enemy.setFillColor(sf::Color(160, 80, 200));
-				}
+
+				}	
 
 			}
+			if (birdReady && event.type == sf::Event::MouseButtonPressed)
+				{
+					if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						sf::Vector2i mousePixel(event.mouseButton.x, event.mouseButton.y);
+						sf::Vector2f mousePos = window.mapPixelToCoords(mousePixel);
+
+						if (bird.getGlobalBounds().contains(mousePos.x, mousePos.y))
+						{
+							isDragging = true;
+						}
+					}
+				}
+
+			if (birdReady && isDragging && event.type == sf::Event::MouseMoved)
+			{
+				sf::Vector2i mousePixel(event.mouseMove.x, event.mouseMove.y);
+				sf::Vector2f mousePos = window.mapPixelToCoords(mousePixel);
+
+				sf::Vector2f desiredPos(mousePos.x - 20.f, mousePos.y - 20.f);
+				sf::Vector2f offset = desiredPos - birdStartPos;
+
+				float distance = std::sqrt(offset.x * offset.x + offset.y * offset.y);
+
+				if (distance > maxDragDistance && distance > 0.f)
+				{
+					offset.x = offset.x / distance * maxDragDistance;
+					offset.y = offset.y / distance * maxDragDistance;
+					desiredPos = birdStartPos + offset;
+				}
+				
+				bird.setPosition(desiredPos);
+			}
+
+			if (birdReady && isDragging && event.type == sf::Event::MouseButtonReleased)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					birdReady = false;
+					isDragging = false;
+
+					birdSpeedX = -(bird.getPosition().x - birdStartPos.x) * launchPower;
+					birdSpeedY = -(bird.getPosition().y - birdStartPos.y) * launchPower;
+
+				}
+			}
+
 		}
 
 		// ===== 更新区 =====
